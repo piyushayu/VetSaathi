2c21d6ba2f10664670928bec30bc1ff2b8eac0b0
piyushayu <piyush.ps1412@outlook.com>
2026-07-09 00:49:50 +0530

    Animalfeeduicompleted

--- Frontend/freevet/src/components/Diseases/Diseases.jsx
+++ Frontend/freevet/src/components/Diseases/Diseases.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Header'
 import Animalcard from './Animalcard'
 import { useId } from 'react'
+import Page from './page'
+import { MdWarning } from 'react-icons/md'
 
 const Allanimals = [
   {
@@ -57,21 +59,58 @@ const Allanimals = [
 ]
 
 
+// const [ warn , setWarn] = useState(true)
+
+// function Warninginfo(){
+//   return (
+//     setWarn(false)
+//   )
+// }
+
+
 function Diseases() {
+
+  const [ warn , setWarn] = useState(false)
+
+  useEffect(() => {
+   const seen = localStorage.getItem("disclaimer")
+   if(!seen){
+    setWarn(true)
+    setTimeout(() => 
+      { setWarn(false)
+        localStorage.setItem("disclaimer" , "true")
+      }, 4000)
+   }
+  },[])
+
 
   const id = useId()
   return (
-    <div className='flex flex-col gap-5'>
+    <div className='flex flex-col gap-5 justify-center '>
         <Header className="w-auto mx-5 mt-3" />
-        <div className='flex flex-row'>
-        {
-          Allanimals.map((animals) => (
-            <Animalcard Name={animals.name} Info={animals.Info} id={id}/>
-          ))
-        }
+
+    <div className='flex flex-row flex-wrap justify-center gap-6 w-full'>
+  {Allanimals.slice(0, 5).map((animals) => (
+    <div key={animals.name} className='w-full max-w-sm flex justify-center'>
+      <Animalcard Name={animals.name} Info={animals.Info} id={id} />
+          </div>
+  ))}
+        </div>
+        <div className='relative flex justify-center items-center w-full px-5'>
+          <Page/>
+          <div className='absolute right-4 flex flex-col items-end '>
+            { warn && <div className='h-25 w-70 bg-gray-600 text-black overflow-hidden p-2 rounded shadow mb-1'>
+              <p className="text-xs tracking-wide"> All information on this site is for 
+               educational purposes only. 
+              Always consult a licensed veterinarian 
+              before giving any medicine to your animal.</p>
+            </div>}
+            <MdWarning onClick={() => setWarn(!warn)} size={24} color="red" className="cursor-pointer" />
+          </div>
         </div>
     </div>
   )
 }
 
 export default Diseases
+
